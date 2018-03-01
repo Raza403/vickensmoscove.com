ee010a4e23e6ff77fd41b1e88157d4a25d002b9f
Ahmed Raza <raza403@gmail.com>
2018-03-01 17:31:06 +0500

    Work started on Vickensmoscova.com

---
+++ header.js
@@ -0,0 +1,12 @@
+<a href="javascript:void(
+  window.open(
+    'https://form.myjotform.com/73124562627557',
+    'blank',
+    'scrollbars=yes,
+    toolbar=no,
+    width=700,
+    height=500'
+  )
+)">
+Viksum
+</a>
